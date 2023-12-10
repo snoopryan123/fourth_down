de69ab2f4867b0d8e29e135d830c322af4fb3e62
Ryan Brill <ryansbrill@gmail.com>
2023-12-09 17:30:20 -0800

    B stability analysis

--- 2_Decision_Making/D4_stability_analysis_run_getJobOutput.sh
+++ 2_Decision_Making/D4_stability_analysis_run_getJobOutput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv job_output/stability_analysis_ddf_*_B=100*     			../../../Dropbox/HPCC/
+mv job_output/stability_analysis_ddf_*_B=100*     			../../Dropbox/HPCC/
 boxup
-mv ../../../Dropbox/HPCC/stability_analysis_ddf_*     	job_output
+mv ../../Dropbox/HPCC/stability_analysis_ddf_*     	job_output
 
